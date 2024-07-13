fa2624dc6d7e334f2486de73c1a13091c7469147
Julián Eduardo Aguirre <aguirre.julian.eduardo@gmail.com>
2024-07-13 17:00:42 -0300

    Created method for checking previous existency for Topico

--- src/main/java/com/alura/forohub/controller/TopicoController.java
+++ src/main/java/com/alura/forohub/controller/TopicoController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/topicos")
 public class TopicoController {
@@ -18,8 +20,15 @@ public class TopicoController {
 
     @PostMapping
     public void registrarTopico(@RequestBody DatosRegistroTopico datosRegistroTopico) {
-        System.out.println("Exito");
-        System.out.println(datosRegistroTopico);
-        topicoRepository.save(new Topico(datosRegistroTopico));
+
+        var repetido = topicoRepository.existsByTituloAndMensaje(datosRegistroTopico.titulo(), datosRegistroTopico.mensaje());
+
+        if (repetido) {
+            System.out.println("Tópico y mensaje repetidos.");
+        } else {
+            System.out.println("Nuevo tópico salvado en la base de datos");
+            topicoRepository.save(new Topico(datosRegistroTopico));
+        }
+
     }
 }
--- src/main/java/com/alura/forohub/repository/TopicoRepository.java
+++ src/main/java/com/alura/forohub/repository/TopicoRepository.java
@@ -4,6 +4,10 @@ import com.alura.forohub.model.Topico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+    boolean existsByTituloAndMensaje(String titulo, String mensaje);
 }
